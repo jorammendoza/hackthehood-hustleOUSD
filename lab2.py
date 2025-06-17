13d4218edaf767f6d3e450c7aee49ad0d5a18872
Joram Mendoza <jorammendoza@Jorams-MBP.attlocal.net>
2025-06-17 13:53:25 -0700

    completed lab 2

---
+++ lab2.py
@@ -0,0 +1,20 @@
+# Lab 2: Python Profiles
+
+## Step 1
+name = "Joram Mendoza" 
+
+## Step 2
+nickname = 'Joram'
+
+## Step 3 
+age = 17
+
+## Step 4
+used_python = False
+
+## Step 5
+hobbies = ["Playing sports", "working out", "eating", "playing video games","hanging out with friends and family" ]
+
+## Step 6 
+favorite_things = {"sports": "soccer", "soccer team": "Real Madrid", "food": "burritos", "color": "black", "animal": "lizards",}
+
